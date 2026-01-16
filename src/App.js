fe7bb1370204a5ada62d9f88ab83d3077db6582a
saketkhundia <saketkhundia21@gmail.com>
2026-01-16 13:00:52 +0530

    Replace portfolio photo

--- src/App.js
+++ src/App.js
@@ -89,7 +89,7 @@ function App() {
       {/* Home */}
       <section id="home" className="home">
         <motion.img
-          src="https://raw.githubusercontent.com/saketkhundia/Saketkhundia.site/main/public/photo.jpg"
+         src = "https://raw.githubusercontent.com/saketkhundia/Saketkhundia.site/main/public/photo.jpg"
           alt="Saket"
           className="profile-pic"
           initial={{ scale: 0 }}
